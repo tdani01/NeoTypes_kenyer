269b539dc8c99f0deab8f258dc25cf53ae4d598b
zanadoman <domanseven@gmail.com>
2023-12-18 23:34:50 +0100

    .

--- Sample/main.c
+++ Sample/main.c
@@ -5,14 +5,10 @@
 sint32 main(sint32 argc, char* *argv)
 {
     string str;
-    char* result;
 
     str = strNew();
     strInit(str, "Hello World!");
-
-    strPurgeKeepString(str, &result);
-    printf("%s\n", result);
-
+    printf("%s\n", str->String);
 
     return 0;
 }
